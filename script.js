fdc6f3ce0b4de567969ae077e16e44b34384b135
Jan Polak <jan.polak@onmyway.cz>
2023-12-14 18:55:12 +0100

    successfully loading data from endpoint

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const loadExchangeRates = async () => {
 
-    console.log('Page loaded, loading exchange rates');
+    const response = await fetch('https://classes.codingbootcamp.cz/assets/classes/exchange-rates/index.php');
+
+    const data = await response.json();
+
+    console.log(data);
 
 }
 
